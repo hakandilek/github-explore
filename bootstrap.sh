8b5c6cb7f0483723c2ba6d609b1e3267053f54fa
Hakan Dilek <hakandilek@gmail.com>
2016-01-24 15:31:12 +0100

    add demo app with angular material starter-app

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 
-#sudo apt-get install -y python-software-properties python g++ make
 sudo add-apt-repository ppa:chris-lea/node.js
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y git-core
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-#echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
 sudo apt-get update
 
-# skip mongo db
-#sudo apt-get install -y mongodb-org
-
+# install npm
 sudo apt-get install -g npm
 
+# install npm modules
 sudo npm install -g npm
 sudo npm install -g bower
 sudo npm install -g gulp
 sudo npm install -g grunt-cli
 sudo npm install -g express
 sudo npm install -g yo
+sudo npm install -g jspm
+
+#initialize demo project
+cd demo
+npm install
