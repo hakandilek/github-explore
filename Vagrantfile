9887f540156ab5b0be29ad2b36b18076db07c5ca
Hakan Dilek <hakandilek@gmail.com>
2016-01-24 16:55:19 +0100

    use smb to support long filenames for npm

--- Vagrantfile
+++ Vagrantfile
@@ -3,6 +3,7 @@
 
 Vagrant.configure(2) do |config|
 	config.vm.box = "ubuntu/trusty64"
+	config.vm.synced_folder ".", "/vagrant", type: "smb"
 	config.vm.provision :shell, :privileged => false, :path => "bootstrap.sh"
 	config.vm.network :forwarded_port, guest: 8000, host: 8000
         config.vm.provider "virtualbox" do |v|
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sudo apt-get install -y python-software-properties python g++ make
 sudo add-apt-repository ppa:chris-lea/node.js
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 sudo apt-get install -y nodejs
@@ -9,7 +10,7 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 sudo apt-get update
 
 # install npm
-sudo apt-get install -g npm
+sudo apt-get install npm
 
 # install npm modules
 sudo npm install -g npm
@@ -19,7 +20,9 @@ sudo npm install -g grunt-cli
 sudo npm install -g express
 sudo npm install -g yo
 sudo npm install -g jspm
+sudo npm install -g bower
+
 
 #initialize demo project
-cd demo
-npm install
+cd /vagrant/demo
+npm install --no-bin-links
